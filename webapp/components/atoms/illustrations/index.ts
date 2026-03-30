99412a5d072ac1b1891b1655f42aa532ee563e8d
Hataf Atif <hatafbinatif@gmail.com>
2026-03-30 14:41:55 +0500

    feat(webapp): add six SVG illustration atoms

---
+++ webapp/components/atoms/illustrations/index.ts
@@ -0,0 +1,6 @@
+export { IllusQuill } from "./illus-quill";
+export { IllusBooks } from "./illus-books";
+export { IllusOpenBook } from "./illus-open-book";
+export { IllusKey } from "./illus-key";
+export { IllusCompass } from "./illus-compass";
+export { IllusComponents } from "./illus-components";
